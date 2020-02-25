7e52a123fd1adfc1efd975d92ee5eb37def3a1bc
Marshall Sykes <rmsykes91@gmail.com>
2020-02-25 12:26:49 -0500

    added toggle for form with jquery for each bracket size

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // forms displayed on click
 
 $(document).ready(function() {
-    $(".formButton").click(function() {
+    $("#fourFormButton").click(function() {
       $("#fourForm").toggle();
     });
   });
 
+  $(document).ready(function() {
+    $("#eightFormButton").click(function() {
+      $("#eightForm").toggle();
+    });
+  });
+
+  $(document).ready(function() {
+    $("#sixteenFormButton").click(function() {
+      $("#sixteenForm").toggle();
+    });
+  });
+
+  $(document).ready(function() {
+    $("#thirtyTwoFormButton").click(function() {
+      $("#thrityTwoForm").toggle();
+    });
+  });
