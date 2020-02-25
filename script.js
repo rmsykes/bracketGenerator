6259b19c39e90ec6185c36b11aa652d5bc5b40b9
Marshall Sykes <rmsykes91@gmail.com>
2020-02-25 13:16:01 -0500

    able to type in an input for fourFormInputOne, click create bracket, and have that value render. need to extend to the other elements of the four form and then render it in a way that looks like a bracket

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-// forms displayed on click
+// objects
+let fourTeamBracket = []
 
+
+
+// hidden forms displayed on click
+
+// 4 team form
 $(document).ready(function() {
     $("#fourFormButton").click(function() {
       $("#fourForm").toggle();
     });
   });
-
+// 8 team form
   $(document).ready(function() {
     $("#eightFormButton").click(function() {
       $("#eightForm").toggle();
     });
   });
-
+// 16 team form
   $(document).ready(function() {
     $("#sixteenFormButton").click(function() {
       $("#sixteenForm").toggle();
     });
   });
-
+// 32 team form
   $(document).ready(function() {
     $("#thirtyTwoFormButton").click(function() {
       $("#thrityTwoForm").toggle();
     });
   });
+
+// create bracket click event listener
+// 4 team create click
+  document.querySelector('#submitFourBracket').addEventListener('click', (e) => {
+    e.preventDefault()
+    fourTeamBracket.push(document.querySelector('#fourFormInputOne').value)
+    document.querySelector('#fourTeamBracket').innerText = fourTeamBracket[0]
+  })
