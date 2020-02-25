7306a6e80672f79c42d4020b3433e6df17a3e7d2
Marshall Sykes <rmsykes91@gmail.com>
2020-02-25 15:19:32 -0500

    added shuffle function, shuffles and returns array whenc called

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 let fourTeamBracket = []
 
 
+// functions
+// shuffle/randomize array
+function shuffle(array) {
+    let m = array.length, t, i;
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
+
 
 // hidden forms displayed on click
 
@@ -35,5 +51,12 @@ $(document).ready(function() {
   document.querySelector('#submitFourBracket').addEventListener('click', (e) => {
     e.preventDefault()
     fourTeamBracket.push(document.querySelector('#fourFormInputOne').value)
-    document.querySelector('#fourTeamBracket').innerText = fourTeamBracket[0]
+    fourTeamBracket.push(document.querySelector('#fourFormInputTwo').value)
+    fourTeamBracket.push(document.querySelector('#fourFormInputThree').value)
+    fourTeamBracket.push(document.querySelector('#fourFormInputFour').value)
+
+    // calls shuffle function from above to shuffle four-team Array
+    shuffle(fourTeamBracket)
+
+    document.querySelector('#fourTeamBracket').innerText = fourTeamBracket
   })
